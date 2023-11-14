b558bd774613f63a708ebe2b5a0a5650cef1dbfd
TomoNanri <131641585+TomoNanri@users.noreply.github.com>
2023-11-14 17:51:16 +0900

    [update] レベル難易度修正

--- Assets/Scripts/LevelData/LevelDataAsset.cs
+++ Assets/Scripts/LevelData/LevelDataAsset.cs
@@ -10,14 +10,14 @@ public class LevelDataAsset : ScriptableObject
     public int StageLevel => _stageLevel;
     public GameObject TargetPrefab => _targetPrefab;
     public GameObject Obstacle1 => _obstacle1;
+    public Vector3 Obs1Position => _obs1Position;
     public GameObject Obstacle2 => _obstacle2;
+    public Vector3 Obs2Position => _obs2Position;
 
-    [SerializeField]
-    private int _stageLevel;
-    [SerializeField]
-    private GameObject _targetPrefab;
-    [SerializeField]
-    private GameObject _obstacle1;
-    [SerializeField]
-    private GameObject _obstacle2;
+    [SerializeField] private int _stageLevel;
+    [SerializeField] private GameObject _targetPrefab;
+    [SerializeField] private GameObject _obstacle1;
+    [SerializeField] private Vector3 _obs1Position;
+    [SerializeField] private GameObject _obstacle2;
+    [SerializeField] private Vector3 _obs2Position;
 }
