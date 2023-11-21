4471a8a6600d57deac96be3931c6d36372f64eac
TomoNanri <131641585+TomoNanri@users.noreply.github.com>
2023-11-21 14:12:28 +0900

    [update] 画面暗転処理追加

--- Assets/Scripts/EndingPanel.cs
+++ Assets/Scripts/EndingPanel.cs
@@ -4,22 +4,27 @@ using System.Text.RegularExpressions;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndingPanel : MonoBehaviour
 {
     [SerializeField] float _LineAppearInterval = 4.0f;
-    [SerializeField] int _moveCount = 1730;
+    [SerializeField] int _moveCount = 1750;
+    [SerializeField] float _volume = 0.4f;
 
     private GameManager _gM;
     private bool _isEndRollActive = false;
     private List<GameObject> _endingTexts = new List<GameObject>();
     private AudioSource _audioSource;
+    private Image _image;
 
     // Start is called before the first frame update
     void Start()
     {
         _gM = GameManager.Instance;
         _audioSource = GetComponent<AudioSource>();
+        _image = GetComponent<Image>();
+
         foreach(Transform t in this.transform)
         {
             if (Regex.IsMatch(t.name, "EndingText"))
@@ -36,6 +41,7 @@ public class EndingPanel : MonoBehaviour
         if(_gM.IsEndingStarted && !_isEndRollActive)
         {
             _isEndRollActive=true;
+            _audioSource.volume = _volume;
             _audioSource.Play();
             StartCoroutine(PlayEnding(_LineAppearInterval, _moveCount));
         }
@@ -51,7 +57,7 @@ public class EndingPanel : MonoBehaviour
             yield return new WaitForSeconds(sec);
             go.SetActive(true);
         }
-        Vector3 deltaPos = new Vector3(0, 1, 0); 
+        Vector3 deltaPos = new Vector3(0, 2, 0); 
         for(int i=0;i<count; i++)
         {
             yield return new WaitForEndOfFrame();
